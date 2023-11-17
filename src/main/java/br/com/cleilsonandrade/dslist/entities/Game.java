6b5f50377da67599d48cceaca921af34638ae104
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-17 11:55:04 -0300

    fix(entity): fix type column descriptions

--- src/main/java/br/com/cleilsonandrade/dslist/entities/Game.java
+++ src/main/java/br/com/cleilsonandrade/dslist/entities/Game.java
@@ -21,7 +21,11 @@ public class Game {
   private String platforms;
   private Double score;
   private String imgUrl;
+
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
   public Game() {
