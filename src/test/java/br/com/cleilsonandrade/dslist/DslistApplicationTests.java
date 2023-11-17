dad73d028f238f6981c7d2531953a2f64a9db1c7
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-17 00:41:18 -0300

    first commit

---
+++ src/test/java/br/com/cleilsonandrade/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleilsonandrade.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
