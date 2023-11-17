47d22f430963931bf582f5e0d11874ea78a42e42
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-17 01:01:23 -0300

    create entity Game

---
+++ src/main/java/br/com/cleilsonandrade/dslist/entities/Game.java
@@ -0,0 +1,131 @@
+package br.com.cleilsonandrade.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "Game")
+@Table(name = "tb_game")
+public class Game {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String tile;
+
+  @Column(name = "game_year")
+  private Integer year;
+  private String genre;
+  private String platform;
+  private String imgUrl;
+  private String shortDescription;
+  private String longDescription;
+
+  public Game() {
+
+  }
+
+  public Game(Long id, String tile, Integer year, String genre, String platform, String imgUrl, String shortDescription,
+      String longDescription) {
+    this.id = id;
+    this.tile = tile;
+    this.year = year;
+    this.genre = genre;
+    this.platform = platform;
+    this.imgUrl = imgUrl;
+    this.shortDescription = shortDescription;
+    this.longDescription = longDescription;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTile() {
+    return tile;
+  }
+
+  public void setTile(String tile) {
+    this.tile = tile;
+  }
+
+  public Integer getYear() {
+    return year;
+  }
+
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+
+  public String getPlatform() {
+    return platform;
+  }
+
+  public void setPlatform(String platform) {
+    this.platform = platform;
+  }
+
+  public String getImgUrl() {
+    return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+    this.imgUrl = imgUrl;
+  }
+
+  public String getShortDescription() {
+    return shortDescription;
+  }
+
+  public void setShortDescription(String shortDescription) {
+    this.shortDescription = shortDescription;
+  }
+
+  public String getLongDescription() {
+    return longDescription;
+  }
+
+  public void setLongDescription(String longDescription) {
+    this.longDescription = longDescription;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Game other = (Game) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+}
