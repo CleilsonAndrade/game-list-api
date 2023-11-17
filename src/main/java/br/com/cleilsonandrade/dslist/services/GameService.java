a647c0545e0fad358bb76e335640707d36380dcf
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-11-17 14:49:54 -0300

    refactor(service): convert return method findAll

--- src/main/java/br/com/cleilsonandrade/dslist/services/GameService.java
+++ src/main/java/br/com/cleilsonandrade/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.cleilsonandrade.dslist.dto.GameMinDTO;
 import br.com.cleilsonandrade.dslist.entities.Game;
 import br.com.cleilsonandrade.dslist.repositories.GameRepository;
 
@@ -13,9 +14,9 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
-  public List<Game> findAll() {
-    var result = gameRepository.findAll();
-
-    return result;
+  public List<GameMinDTO> findAll() {
+    List<Game> result = gameRepository.findAll();
+    List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+    return dto;
   }
 }
